1f717895bebb641a6826d8d6ba2c2c3420d1dfa9
Stacy Yoshimitsu <itsmest888@yahoo.com>
2021-02-06 15:48:48 +0000

    Add logic and test

---
+++ src/main/java/com/simplilearn/Calculator1/Calculator1Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calculator1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/Calculator1/Calculator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.Calculator1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
